04de1f19b7e90e268c5689c5a37a6cd47c67bd86
dhwz <dhwz@gulli.com>
2013-07-13 11:48:20 +0200

    fixed Doku.me (plugin may be removed soon, most videos don't work)

--- additions/dokume.py
+++ additions/dokume.py
@@ -1,55 +1,10 @@
 from Plugins.Extensions.MediaPortal.resources.imports import *
-from Plugins.Extensions.MediaPortal.resources.decrypt import *
+from Plugins.Extensions.MediaPortal.resources.yt_url import *
 
-class dokumeFilmeListEntry(GUIComponent, object):
-	GUI_WIDGET = eListbox
-	
-	def __init__(self):
-		GUIComponent.__init__(self)
-		self.l = eListboxPythonMultiContent()
-		self.l.setBuildFunc(self.buildList)
-		self.l.setFont(0, gFont("mediaportal", 22))
-		self.l.setFont(1, gFont("mediaportal", 18))
-		self.l.setItemHeight(25)
-
-	def buildList(self, entry):
-		res = [ None ]
-		width = self.l.getItemSize().width()
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0]))
-		return res
-		
-	def getCurrent(self):
-		cur = self.l.getCurrentSelection()
-		return cur and cur[0]
-
-	def postWidgetCreate(self, instance):
-		instance.setContent(self.l)
-		self.instance.setWrapAround(True)
-
-	def preWidgetRemove(self, instance):
-		instance.setContent(None)
-
-	def setList(self, list):
-		self.l.setList(list)
-
-	def moveToIndex(self, idx):
-		self.instance.moveSelectionTo(idx)
-		
-	def pageUp(self):
-		if self.instance is not None:
-			self.instance.moveSelection(self.instance.pageUp)
-
-	def pageDown(self):
-		if self.instance is not None:
-			self.instance.moveSelection(self.instance.pageDown)
-
-	def up(self):
-		if self.instance is not None:
-			self.instance.moveSelection(self.instance.moveUp)
-
-	def down(self):
-		if self.instance is not None:
-			self.instance.moveSelection(self.instance.moveDown)
+def auswahlListEntry(entry):
+	return [entry,
+		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0])
+		]
 
 class dokuScreen(Screen):
 	
@@ -71,16 +26,24 @@ class dokuScreen(Screen):
 			"up" : self.keyUp,
 			"down" : self.keyDown,
 			"right" : self.keyRight,
-			"left" : self.keyLeft
+			"left" : self.keyLeft,
+			"yellow" : self.keyVideoQuality
 		}, -1)
 
-		self['title'] = Label("Doku.me")
-		self['name'] = Label("Dokumentation Auswahl")
+		self['name'] = Label("Doku.me")
+		self['title'] = Label("Dokumentation Auswahl")
 		self['coverArt'] = Pixmap()
 		self.keyLocked = False
+		self.videoPrio = int(config.mediaportal.youtubeprio.value)
+		self.videoPrioS = ['Low','Medium', 'High']
+		self['name'].setText('Doku.me (Video Quality: ' + self.videoPrioS[self.videoPrio] + ')')
 		
 		self.filmliste = []
-		self["genreList"] = dokumeFilmeListEntry()
+		
+		self.chooseMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
+		self.chooseMenuList.l.setFont(0, gFont('mediaportal', 23))
+		self.chooseMenuList.l.setItemHeight(25)
+		self['genreList'] = self.chooseMenuList
 		
 		self.onLayoutFinish.append(self.layoutFinished)
 		
@@ -89,182 +52,69 @@ class dokuScreen(Screen):
 		url = "http://doku.me/liste-aller-dokumentationen"
 		getPage(url, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.allData).addErrback(self.dataError)
 	
+	def keyVideoQuality(self):
+		if self.videoPrio+1 > 2:
+			self.videoPrio = 0
+		else:
+			self.videoPrio += 1
+		self['name'].setText('Doku.me (Video Quality: ' + self.videoPrioS[self.videoPrio] + ')')
+
 	def allData(self, data):
-		print "hole doku daten.."
-		dkAZ = re.findall('<li><a href="(http://doku.me/.*?)" ><span class="head">(.*?)</span></a></li>', data)
-		print "doku parsing ist fertig.."
+		dkAZ = re.findall('<li><a\shref="(http://doku.me/.*?)"\s{0,2}><span\sclass="head">(.*?)</span></a></li>', data)
 		if dkAZ:
 			for (dkUrl,dkTitle) in dkAZ:
 				if not re.match('.*?(\[|"|\(|/|//|///)', dkTitle[0]):
-					self.filmliste.append(((dkTitle, dkUrl),))
-			self["genreList"].setList(self.filmliste)
+					self.filmliste.append((dkTitle, dkUrl))
+			self.chooseMenuList.setList(map(auswahlListEntry, self.filmliste))
+			self.chooseMenuList.moveToIndex(0)
 			self.keyLocked = False
-			self['name'] = Label("Dokumentation Auswahl \(%s\)" % len(dkAZ))
-			#self.showInfos()
 
 	def dataError(self, error):
 		print error
 
-	def showInfos(self):
-		pass
-		#nkTitle = self['genreList'].getCurrent()[0][0]
-		#nkImage = self['genreList'].getCurrent()[0][1]
-		#print nkImage
-		#self['name'].setText(nkTitle)
-		#downloadPage(nkImage, "/tmp/nkIcon.jpg").addCallback(self.ShowCover)
-		
-	def ShowCover(self, picData):
-		if fileExists("/tmp/nkIcon.jpg"):
-			self['coverArt'].instance.setPixmap(gPixmapPtr())
-			self.scale = AVSwitch().getFramebufferScale()
-			self.picload = ePicLoad()
-			size = self['coverArt'].instance.size()
-			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
-			if self.picload.startDecode("/tmp/nkIcon.jpg", 0, 0, False) == 0:
-				ptr = self.picload.getData()
-				if ptr != None:
-					self['coverArt'].instance.setPixmap(ptr)
-					self['coverArt'].show()
-					del self.picload
-	
-	def getVideoUrl(self, url):
-		# this part is from mtube plugin
-		print "got url:", url
-		VIDEO_FMT_PRIORITY_MAP = {
-			'38' : 1, #MP4 Original (HD)
-			'37' : 2, #MP4 1080p (HD)
-			'22' : 3, #MP4 720p (HD)
-			'18' : 4, #MP4 360p
-			'35' : 5, #FLV 480p
-			'34' : 6, #FLV 360p
-		}
-		video_url = None
-		video_id = url
-
-		# Getting video webpage
-		#URLs for YouTube video pages will change from the format http://www.youtube.com/watch?v=ylLzyHk54Z0 to http://www.youtube.com/watch#!v=ylLzyHk54Z0.
-		watch_url = 'http://www.youtube.com/watch?v=%s&gl=US&hl=en' % video_id
-		watchrequest = Request(watch_url, None, std_headers)
-		try:
-			print "[MyTube] trying to find out if a HD Stream is available",watch_url
-			watchvideopage = urlopen2(watchrequest).read()
-		except (URLError, HTTPException, socket.error), err:
-			print "[MyTube] Error: Unable to retrieve watchpage - Error code: ", str(err)
-			print "test", video_url
-
-		# Get video info
-		for el in ['&el=embedded', '&el=detailpage', '&el=vevo', '']:
-			info_url = ('http://www.youtube.com/get_video_info?&video_id=%s%s&ps=default&eurl=&gl=US&hl=en' % (video_id, el))
-			request = Request(info_url, None, std_headers)
-			try:
-				infopage = urlopen2(request).read()
-				videoinfo = parse_qs(infopage)
-				if ('url_encoded_fmt_stream_map' or 'fmt_url_map') in videoinfo:
-					break
-			except (URLError, HTTPException, socket.error), err:
-				print "[MyTube] Error: unable to download video infopage",str(err)
-				return video_url
-
-		if ('url_encoded_fmt_stream_map' or 'fmt_url_map') not in videoinfo:
-			# Attempt to see if YouTube has issued an error message
-			if 'reason' not in videoinfo:
-				print '[MyTube] Error: unable to extract "fmt_url_map" or "url_encoded_fmt_stream_map" parameter for unknown reason'
-			else:
-				reason = unquote_plus(videoinfo['reason'][0])
-				print '[MyTube] Error: YouTube said: %s' % reason.decode('utf-8')
-			print video_url
-
-		video_fmt_map = {}
-		fmt_infomap = {}
-		if videoinfo.has_key('url_encoded_fmt_stream_map'):
-			tmp_fmtUrlDATA = videoinfo['url_encoded_fmt_stream_map'][0].split(',')
-		else:
-			tmp_fmtUrlDATA = videoinfo['fmt_url_map'][0].split(',')
-		for fmtstring in tmp_fmtUrlDATA:
-			fmturl = fmtid = fmtsig = ""
-			if videoinfo.has_key('url_encoded_fmt_stream_map'):
-				try:
-					for arg in fmtstring.split('&'):
-						if arg.find('=') >= 0:
-							print arg.split('=')
-							key, value = arg.split('=')
-							if key == 'itag':
-								if len(value) > 3:
-									value = value[:2]
-								fmtid = value
-							elif key == 'url':
-								fmturl = value
-							elif key == 'sig':
-								fmtsig = value
-								
-					if fmtid != "" and fmturl != "" and fmtsig != ""  and VIDEO_FMT_PRIORITY_MAP.has_key(fmtid):
-						video_fmt_map[VIDEO_FMT_PRIORITY_MAP[fmtid]] = { 'fmtid': fmtid, 'fmturl': unquote_plus(fmturl), 'fmtsig': fmtsig }
-						fmt_infomap[int(fmtid)] = "%s&signature=%s" %(unquote_plus(fmturl), fmtsig)
-					fmturl = fmtid = fmtsig = ""
-
-				except:
-					print "error parsing fmtstring:",fmtstring
-					
-			else:
-				(fmtid,fmturl) = fmtstring.split('|')
-			if VIDEO_FMT_PRIORITY_MAP.has_key(fmtid) and fmtid != "":
-				video_fmt_map[VIDEO_FMT_PRIORITY_MAP[fmtid]] = { 'fmtid': fmtid, 'fmturl': unquote_plus(fmturl) }
-				fmt_infomap[int(fmtid)] = unquote_plus(fmturl)
-		print "[MyTube] got",sorted(fmt_infomap.iterkeys())
-		if video_fmt_map and len(video_fmt_map):
-			print "[MyTube] found best available video format:",video_fmt_map[sorted(video_fmt_map.iterkeys())[0]]['fmtid']
-			best_video = video_fmt_map[sorted(video_fmt_map.iterkeys())[0]]
-			video_url = "%s&signature=%s" %(best_video['fmturl'].split(';')[0], best_video['fmtsig'])
-			print "[MyTube] found best available video url:",video_url
-
-		return video_url
-		
 	def keyLeft(self):
 		if self.keyLocked:
 			return
 		self['genreList'].pageUp()
-		self.showInfos()
 		
 	def keyRight(self):
 		if self.keyLocked:
 			return
 		self['genreList'].pageDown()
-		self.showInfos()
 		
 	def keyUp(self):
 		if self.keyLocked:
 			return
 		self['genreList'].up()
-		self.showInfos()
 		
 	def keyDown(self):
 		if self.keyLocked:
 			return
 		self['genreList'].down()
-		self.showInfos()
 		
 	def keyOK(self):
 		if self.keyLocked:
 			return
-		dkLink = self['genreList'].getCurrent()[1]
+		dkLink = self['genreList'].getCurrent()[0][1]
 		print dkLink
-		getPage(dkLink, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.catchYoutTubeUrl).addErrback(self.dataError)
+		getPage(dkLink, headers={'Content-Type':'application/x-www-form-urlencoded'}).addCallback(self.getVideoPage).addErrback(self.dataError)
 		
-	def catchYoutTubeUrl(self, data):
-		dkTitle = self['genreList'].getCurrent()[0]
-		youtubeID = re.findall('"http://www.youtube.com/embed/(.*?)"', data, re.S)
-		if youtubeID:
-			dkLink = self.getVideoUrl(youtubeID[0].replace('?rel=0',''))
-			if dkLink:
-				print dkLink
-				sref = eServiceReference(0x1001, 0, dkLink)
-				sref.setName(dkTitle)
-				self.session.open(MoviePlayer, sref)
+	def getVideoPage(self, data):
+		videoPage = re.findall('"http://www.youtube.com/(v|embed)/(.*?)\?.*?"', data, re.S)
+		if videoPage:
+			print videoPage
+			url = youtubeUrl(self.session).getVideoUrl(videoPage[0][1], self.videoPrio)
+			if url:
+				self.play(url)
+		else:
+			message = self.session.open(MessageBox, _("Dieses Video ist nicht verfuegbar."), MessageBox.TYPE_INFO, timeout=5)
+		self.keyLocked = False
+					
+	def play(self,file):
+		xxxtitle = self['genreList'].getCurrent()[0][0]
+		sref = eServiceReference(0x1001, 0, file)
+		sref.setName(xxxtitle)
+		self.session.open(MoviePlayer, sref)
 
 	def keyCancel(self):
 		self.close()
-		
-def auswahlListEntry(entry):
-	return [entry,
-		(eListboxPythonMultiContent.TYPE_TEXT, 20, 0, 900, 25, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, entry[0])
-		]
