bedeb85e8c46a08103951658a9976951b1b03168
dhwz <dhwz@gulli.com>
2013-07-21 12:04:07 +0200

    MTV.de fix

--- resources/mtvdelink.py
+++ resources/mtvdelink.py
@@ -22,13 +22,24 @@ class MTVdeLink:
 			self.artist = title[:p].strip()
 			self.title = title[p+3:].strip()
 
+		data = ''
 		url = "http://api.mtvnn.com/v2/mrss?uri=mgid:sensei:video:mtvnn.com:music_video-%s-DE" % token
-		data = urllib.urlopen(url).read()
-		rtmpURL = re.findall("<media:content.*?url='(.*?)'>", data)
-		if rtmpURL:
-			data = urllib.urlopen(rtmpURL[0]).read()
-			rtmpLink = re.findall('<src>(rtmp.*?)</src>', data)
-			
-		videourl = rtmpLink[-1] + ' swfUrl=http://player.mtvnn.com/g2/g2player_2.1.4.swf swfVfy=1'
+		try:
+			data = urlopen2(url).read()
+		except (URLError, HTTPException, socket.error), err:
+			printl(err,self,"E")
+
+		rtmpurl = re.search("<media:content.*?url='(.*?)'>", data)
+		if rtmpurl:
+			try:
+				data = urlopen2(rtmpurl.group(1)).read()
+			except (URLError, HTTPException, socket.error), err:
+				printl(err,self,"E")
+
+		rtmplink = re.findall('<src>(rtmp.*?)</src>', data)
+		if rtmplink:
+			videourl = rtmplink[-1] + ' swfUrl=http://player.mtvnn.com/g2/g2player_2.1.4.swf swfVfy=1'
+		else:
+			videourl = None
 
 		self._callback(self.title, videourl, imgurl=self.imgurl, artist=self.artist)
