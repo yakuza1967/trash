4df6d0f76ac58b7098fc13d81699de03f37d3997
einfall <nix>
2013-07-24 15:38:17 +0200

    cover, handlung added.

--- additions/galileovl.py
+++ additions/galileovl.py
@@ -36,7 +36,7 @@ class galileovlGenreScreen(Screen):
 		}, -1)
 
 		self.keyLocked = True
-		self['title'] = Label("galileo-videolexikon.de")
+		self['title'] = Label("Galileo-Videolexikon.de")
 		self['ContentTitle'] = Label("Auswahl:")
 		self['name'] = Label("")
 		self['F1'] = Label("Exit")
@@ -101,9 +101,9 @@ class galileovlListeScreen(Screen):
 		self.plugin_path = mp_globals.pluginPath
 		self.skin_path =  mp_globals.pluginPath + "/skins"
 
-		path = "%s/%s/defaultGenreScreen.xml" % (self.skin_path, config.mediaportal.skin.value)
+		path = "%s/%s/defaultListWideScreen.xml" % (self.skin_path, config.mediaportal.skin.value)
 		if not fileExists(path):
-			path = self.skin_path + "/original/defaultGenreScreen.xml"
+			path = self.skin_path + "/original/defaultListWideScreen.xml"
 
 		with open(path, "r") as f:
 			self.skin = f.read()
@@ -114,11 +114,15 @@ class galileovlListeScreen(Screen):
 		self["actions"]  = ActionMap(["OkCancelActions", "ShortcutActions", "WizardActions", "ColorActions", "SetupActions", "NumberActions", "MenuActions", "EPGSelectActions"], {
 			"ok"    : self.keyOK,
 			"cancel": self.keyCancel,
-			"red": self.keyCancel
+			"red": self.keyCancel,
+			"up": self.keyUp,
+			"down": self.keyDown,
+			"right": self.keyRight,
+			"left": self.keyLeft,
 		}, -1)
 
 		self.keyLocked = True
-		self['title'] = Label("galileo-videolexikon.de")
+		self['title'] = Label("Galileo-Videolexikon.de")
 		self['ContentTitle'] = Label("Videos - %s" % self.galileovlName)
 		self['name'] = Label("")
 		self['F1'] = Label("Exit")
@@ -127,12 +131,16 @@ class galileovlListeScreen(Screen):
 		self['F4'] = Label("")
 		self['F3'].hide()
 		self['F4'].hide()
+		self['handlung'] = Label("")
+		self['page'] = Label("")
+		self['Page'] = Label("")
+		self['coverArt'] = Pixmap()
 
 		self.videoliste = []
 		self.chooseMenuList = MenuList([], enableWrapAround=True, content=eListboxPythonMultiContent)
 		self.chooseMenuList.l.setFont(0, gFont('mediaportal', 23))
 		self.chooseMenuList.l.setItemHeight(25)
-		self['genreList'] = self.chooseMenuList
+		self['liste'] = self.chooseMenuList
 
 		self.onLayoutFinish.append(self.loadPage)
 
@@ -145,18 +153,64 @@ class galileovlListeScreen(Screen):
 		videos = re.findall('"id":"(.*?)".*?"assestid":"(.*?)".*?"title":"(.*?)".*?"serie":"(.*?)".*?"description":"(.*?)".*?"duration":"(.*?)"', data, re.S )
 		if videos:
 			for (id, videoid, title, date, desc, dur) in videos:
-				print id, videoid, title, date, desc, dur
+				#print id, videoid, title, date, desc, dur
 				title = "%s - %s" % (date, title)
-				self.videoliste.append((decodeHtml(title), videoid, desc))
+				image = "http://www.prosieben.de/dynamic/h264/thumbnails/?res=high&app=galileo&ClipID=%s" % videoid
+				self.videoliste.append((decodeHtml(title), videoid, desc, image))
 
 			self.chooseMenuList.setList(map(galileovlListEntry, self.videoliste))
+			self.loadPic()
 			self.keyLocked = False
 
+	def loadPic(self):
+		streamPic = self['liste'].getCurrent()[0][3]
+		handlung = self['liste'].getCurrent()[0][2]
+		self['handlung'].setText(decodeHtml(handlung))
+		downloadPage(streamPic, "/tmp/Icon.jpg").addCallback(self.ShowCover)
+
+	def ShowCover(self, picData):
+		if fileExists("/tmp/Icon.jpg"):
+			self['coverArt'].instance.setPixmap(gPixmapPtr())
+			self.scale = AVSwitch().getFramebufferScale()
+			self.picload = ePicLoad()
+			size = self['coverArt'].instance.size()
+			self.picload.setPara((size.width(), size.height(), self.scale[0], self.scale[1], False, 1, "#FF000000"))
+			if self.picload.startDecode("/tmp/Icon.jpg", 0, 0, False) == 0:
+				ptr = self.picload.getData()
+				if ptr != None:
+					self['coverArt'].instance.setPixmap(ptr)
+					self['coverArt'].show()
+					del self.picload
+					
+	def keyLeft(self):
+		if self.keyLocked:
+			return
+		self['liste'].pageUp()
+		self.loadPic()
+
+	def keyRight(self):
+		if self.keyLocked:
+			return
+		self['liste'].pageDown()
+		self.loadPic()
+
+	def keyUp(self):
+		if self.keyLocked:
+			return
+		self['liste'].up()
+		self.loadPic()
+
+	def keyDown(self):
+		if self.keyLocked:
+			return
+		self['liste'].down()
+		self.loadPic()
+		
 	def keyOK(self):
 		if self.keyLocked:
 			return
-		self.galileovltitle = self['genreList'].getCurrent()[0][0]
-		self.galileovlid = self['genreList'].getCurrent()[0][1]
+		self.galileovltitle = self['liste'].getCurrent()[0][0]
+		self.galileovlid = self['liste'].getCurrent()[0][1]
 		print self.galileovltitle, self.galileovlid
 
 		url = "http://ws.vtc.sim-technik.de/video/video.jsonp?type=1&app=GalVidLex_web&clipid=%s" % self.galileovlid
@@ -168,10 +222,26 @@ class galileovlListeScreen(Screen):
 		if stream_url:
 			stream_url = stream_url[0].replace('\\','')
 			print stream_url
-			self.session.open(SimplePlayer, [(self.galileovltitle, stream_url)], showPlaylist=False, ltype='galileovl')
+			#sref = eServiceReference(0x1001, 0, stream_url)
+			#sref.setName(self.galileovltitle)
+			#self.session.open(MoviePlayer, sref)
+			self.session.open(galileovlPlayer, [(self.galileovltitle, stream_url)])
 
 	def dataError(self, error):
 		printl(error,self,"E")
 
 	def keyCancel(self):
 		self.close()
+
+
+class galileovlPlayer(SimplePlayer):
+
+	def __init__(self, session, playList):
+		print "galileovlPlayer:"
+
+		SimplePlayer.__init__(self, session, playList, showPlaylist=False)
+
+	def getVideo(self):
+		title = self.playList[self.playIdx][0]
+		url = self.playList[self.playIdx][1]
+		self.playStream(title, url)
