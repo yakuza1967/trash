5980b0be714873a77dcfda3a20a14c86943b7c84
dhwz <dhwz@gulli.com>
2013-07-25 20:59:06 +0200

    skin cleanup

--- additions/failto.py
+++ additions/failto.py
@@ -9,9 +9,9 @@ class failScreen(Screen):
 
 	def __init__(self, session):
 		self.session = session
-		path = "/usr/lib/enigma2/python/Plugins/Extensions/MediaPortal/skins/%s/failScreen.xml" % config.mediaportal.skin.value
+		path = "/usr/lib/enigma2/python/Plugins/Extensions/MediaPortal/skins/%s/roflScreen.xml" % config.mediaportal.skin.value
 		if not fileExists(path):
-			path = "/usr/lib/enigma2/python/Plugins/Extensions/MediaPortal/skins/original/failScreen.xml"
+			path = "/usr/lib/enigma2/python/Plugins/Extensions/MediaPortal/skins/original/roflScreen.xml"
 		print path
 		with open(path, "r") as f:
 			self.skin = f.read()
