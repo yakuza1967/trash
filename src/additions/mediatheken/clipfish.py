890dfa1f881f24e4ea4fb8d701341a28b86dc3d0
Billy2011 <kschmidt2007@googlemail.com>
2013-08-29 17:36:45 +0200

    clipfish: genre fixes

--- src/additions/mediatheken/clipfish.py
+++ src/additions/mediatheken/clipfish.py
@@ -145,7 +145,7 @@ class show_CF_Genre(Screen):
 		self.genreSelected = False
 		self.menuListe = []
 		self.baseUrl = "http://www.clipfish.de"
-		self.genreBase = ["/suche", "/kategorien", "/musikvideos/charts", "/musikvideos/genre","/specialmodule/modulemusicvideodatematrix/5337/%d/?relyear=", "/special/spielfilme/genre", "/special/kino-trailer/home/neu/%d/#111", "/special", "/special", "/special", "/special", "/special", "/special"]
+		self.genreBase = ["/suche", "/kategorien", "/musikvideos/charts", "/musikvideos/genre","/specialmodule/modulemusicvideodatematrix/5337/%d/?relyear=", "/special/spielfilme/genre", "/special/kino-trailer/home/%d/#111", "/special", "/special", "/special", "/special", "/special", "/special"]
 		self.genreName = ["","","",""]
 		self.genreUrl = ["","","",""]
 		self.genreTitle = ""
@@ -223,45 +223,46 @@ class show_CF_Genre(Screen):
 			("Horror", "/27/horror/neu/%d/#27"),
 			("Asian", "/71/asian/neu/%d/#71"),
 			("Erotik", "/25/erotik/neu/%d/#25"),
-			("Komödie", "/29/komoedie/neu/%d/#29")
+			("Komödie", "/29/komoedie/neu/%d/#29"),
+			("Zeichentrick", "/63/zeichentrick/neu/%d/#63")
 			],
 			None,
 			[
-			("Daniele Rizzo - Alle Videos", "/daniele-rizzo/home/neu/%d/"),
-			("Der ehrliche Dennis - Alle Videos", "/der-ehrliche-dennis/home/neu/%d/"),
-			("Seen - Die aktuellen Kino- und DVD-Filme", "/seen/home/neu/%d/"),
-			("Kino und Co. - Alle Videos", "/kino-und-co/home/neu/%d/")
+			("Daniele Rizzo - Alle Videos", "/daniele-rizzo/home/%d/"),
+			("Der ehrliche Dennis - Alle Videos", "/der-ehrliche-dennis/home/%d/"),
+			("Seen - Die aktuellen Kino- und DVD-Filme", "/seen/home/%d/"),
+			("Kino und Co. - Alle Videos", "/kino-und-co/home/%d/")
 			],
 			[
-			("News und Lifestyle - Alle Videos", "/news/aktuelles/neu/%d/"),
-			("Alle VIP-Videos", "/news/vip/neu/%d/"),
-			("Regional News - alle Videos", "/regional-news/home/neu/%d/"),
-			("Neues von Daaruum", "/daaruum/home/neu/%d/")
+			("News und Lifestyle - Alle Videos", "/news/aktuelles/%d/"),
+			("Alle VIP-Videos", "/news/vip/%d/"),
+			("Regional News - alle Videos", "/regional-news/home/%d/"),
+			("Neues von Daaruum", "/daaruum/home/%d/")
 			],
 			[
-			("Y-Titty - Videos", "/y-titty/home/neu/%d/"),
-			("Neues von FreshTorge", "/freshaltefolie/home/neu/%d/"),
-			("Neues von den Lochis", "/dielochis/home/neu/%d/"),
-			("Digges Ding Comedy - Alle Videos", "/digges-ding-comedy/home/neu/%d/"),
-			("ApeCrime - Alle Videos", "/ape-crime/home/neu/%d/")
+			("Y-Titty - Videos", "/y-titty/home/%d/"),
+			("Neues von FreshTorge", "/freshaltefolie/home/%d/"),
+			("Neues von den Lochis", "/dielochis/home/%d/"),
+			("Digges Ding Comedy - Alle Videos", "/digges-ding-comedy/home/%d/"),
+			("ApeCrime - Alle Videos", "/ape-crime/home/%d/")
 			],
 			[
-			("Alle Videos", "/familien-duell/home/neu/%d/")
+			("Alle Videos", "/familien-duell/home/%d/")
 			],
 			[
-			("DSDS 2013 - News", "/dsds/news/neu/%d/"),
-			("DSDS Musikvideos", "/musikvideos/dsds/neu/%d/"),
-			("Alle Videos aus den DSDS-Liveshows 2013", "/special/dsds/liveshow/neu/%d/"),
-			("DSDS Recall 2013 - Alle Videos", "/dsds/recall/neu/%d/"),
-			("DSDS 2013 Alle Casting-Videos", "/dsds/casting/neu/%d/"),
-			("DSDS 2012 - Alle Videos", "/dsds/2012/neu/%d/"),
-			("DSDS 8 - Videos", "/dsds/dsds-8/neu/%d/"),
-			("DSDS 7 - Videos", "/dsds/dsds-7/neu/%d/")
+			("DSDS 2013 - News", "/dsds/news/%d/"),
+			("DSDS Musikvideos", "/musikvideos/dsds/%d/"),
+			("Alle Videos aus den DSDS-Liveshows 2013", "/special/dsds/liveshow/%d/"),
+			("DSDS Recall 2013 - Alle Videos", "/dsds/recall/%d/"),
+			("DSDS 2013 Alle Casting-Videos", "/dsds/casting/%d/"),
+			("DSDS 2012 - Alle Videos", "/dsds/2012/%d/"),
+			("DSDS 8 - Videos", "/dsds/dsds-8/%d/"),
+			("DSDS 7 - Videos", "/dsds/dsds-7/%d/")
 			],
 			[
-			("Supertalent 2012 Backstage - Alle Videos", "/supertalent/supertalent-backstage/neu/%d/"),
-			("Das Supertalent 2012 - Alle Videos", "/supertalent/videos/neu/%d/"),
-			("Das Supertalent 2011 - Alle Videos", "/supertalent/2011/neu/%d/")
+			("Supertalent 2012 Backstage - Alle Videos", "/supertalent/supertalent-backstage/%d/"),
+			("Das Supertalent 2012 - Alle Videos", "/supertalent/videos/%d/"),
+			("Das Supertalent 2011 - Alle Videos", "/supertalent/2011/%d/")
 			]
 			]
 			]
