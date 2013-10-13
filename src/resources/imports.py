38436fa519820f3849eadc37b876c8da224837b5
dhwz <dhwz@gulli.com>
2013-10-13 18:54:31 +0200

    fix

--- src/resources/imports.py
+++ src/resources/imports.py
@@ -34,7 +34,7 @@ from Tools.LoadPixmap import LoadPixmap
 
 from jsunpacker import cJsUnpacker
 
-import re, urllib, urllib2, os, cookielib, time, socket, sha, shutil, base64, datetime, math, hashlib, random, json, md5
+import re, urllib, urllib2, os, cookielib, time, socket, sha, shutil, base64, datetime, math, hashlib, random, json, md5, string
 from urllib2 import Request, URLError, urlopen as urlopen2
 from socket import gaierror, error
 from urllib import quote, unquote_plus, unquote, urlencode
